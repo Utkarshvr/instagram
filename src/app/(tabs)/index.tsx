3bb11734ed95b461cdced052b8329f47b7fa511c
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2025-03-25 13:21:52 +0530

    checking up on cloudinary-react-native

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,39 +1,27 @@
-import {
-  Button,
-  StyleSheet,
-  TextInput,
-  ToastAndroid,
-  TouchableWithoutFeedback,
-} from "react-native";
-import "../../../global.css";
 import { signOut } from "firebase/auth";
+import { AdvancedImage } from "cloudinary-react-native";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { Button, SafeAreaView } from "react-native";
 import { auth } from "../_layout";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { useRef } from "react";
+
+const myCld = new Cloudinary({
+  cloud: {
+    cloudName: "dxxxuk7hc",
+  },
+});
+
+const myImage = myCld.image("pokedex_315bf9a908");
 
 export default function Home() {
-  const inputRef = useRef<TextInput>(null);
-
   return (
-    <SafeAreaView className="bg-neutral-950 flex-1 p-4 gap-5">
+    <SafeAreaView className="bg-neutral-950 flex-1 gap-5">
       <Button
         onPress={() => {
           signOut(auth);
         }}
         title="Log out"
       />
-      <Button title="Clikc" onPress={() => inputRef.current?.focus()} />
-      <TextInput
-        // onChangeText={(txt) => setText(txt)}
-        ref={inputRef}
-        // value={text}
-        className="px-4 py-2 w-full rounded-md bg-neutral-800 text-neutral-50 font-montserrat"
-        placeholder="Search a user"
-        placeholderTextColor={"white"}
-        autoCapitalize="none"
-      />
+      <AdvancedImage cldImg={myImage} style={{ width: "100%", height: 200 }} />
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({});
